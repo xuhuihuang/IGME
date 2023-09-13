1210be1c560c1cce49d8d59e0347723c74c6679f
Siqin Cao <31610952+seechin@users.noreply.github.com>
2023-09-13 12:07:22 -0500

    Add files via upload

--- msmbuilder_compatible_version/igme.py
+++ msmbuilder_compatible_version/igme.py
@@ -1,13 +1,28 @@
+# Author: Siqin Cao <siqincao@gmail.com>
+# Copyright (c) 2021-2023, University of Wisconsin-Madison
+# All rights reserved.
+
 """ Integrative Generalized Master Equation (IGME)
 Use the Least Square fitting algorithm to build IGME model(s) from given
-time-dependent transition probability matrixes. The IGME can provide
+time-dependent transition probability matrics. The IGME can provide
 numerically stable approaches to compute long-time dynamics and time integrated
 memory kernels.
+
+The theory of IGME (http://doi.org/10.26434/chemrxiv-2022-0n9ld-v2):
+    T(t) = A \hat{T}^t
+
+The implementation in this python script:
+  IGME._theory == 0 (default):
+    ln T(t) = ln A + t ln \hat{T} 
+  IGME._theory == 2:
+    constructed with    ln T(t) = ln A + t ln \hat{T} 
+    RMSE computed with  T(t) = A \hat{T}^t
+  IGME._theory == 3:
+    constructed with    ln T(t) = ln A + t ln \hat{T}
+    RMSE computed with  T(t) = A^0.5 \hat{T}^t A^0.5
+The theory behind the default theory will be derived in the theory of spectral
+IGME. 
 """
-# Author: Siqin Cao <siqincao@gmail.com>
-# Contributors:
-# Copyright (c) 2023, University of Wisconsin-Madison
-# All rights reserved.
 
 import numpy
 import sys
@@ -36,7 +51,7 @@ class IGMENS :
             return IGMENS.matrix_log_accurate(m)
 
     @staticmethod
-    def matrixes_log(ms, logarithm_approx_order=0) :
+    def matrics_log(ms, logarithm_approx_order=0) :
         ret = []
         for i in range(len(ms)) :
             ret.append(IGMENS.matrix_log(ms[i], logarithm_approx_order))
@@ -48,44 +63,55 @@ class IGMENS :
         return numpy.matmul(vr, numpy.matmul(numpy.diag(numpy.exp(ev)), numpy.linalg.inv(vr)))
 
     @staticmethod
-    def matrixes_exp(ms) :
+    def matrics_exp(ms) :
         ret = []
         for i in range(len(ms)) :
             ret.append(IGMENS.matrix_exp(ms[i]))
         return ret
 
     @staticmethod
-    def lsf_xy(x, y) :
+    def lsfs_xy(x, y, dimension) :
         length = len(x)
-        if (length>len(y)) :
-            length = len(y)
+        if (length>len(y[0])) :
+            length = len(y[0])
+        M = numpy.zeros([2*dimension+1,2*dimension+1])
+        Z = numpy.zeros([2*dimension+1,1])
+        R = numpy.zeros([dimension])
         sx = numpy.mean(x)
-        sy = numpy.mean(y)
         sxx = numpy.dot(x, x) / length
-        syy = numpy.dot(y, y) / length
-        sxy = numpy.dot(x, y) / length
-        if (syy == sy*sy) :
-            return sy, 0, 1
-        if (sxx == sx*sx) :
-            return sy, 0, 0
-        k = (sxy - sx*sy) / (sxx - sx*sx)
-        b = sy - k * sx
-        r = (sxy - sx*sy) / numpy.sqrt((sxx-sx*sx) * (syy - sy*sy))
-        return b, k, r
+        for i in range(dimension) :
+            M[i,i] = sx
+            M[i+dimension,i] = 1
+            M[i+dimension,i+dimension] = sx
+            M[i,i+dimension] = sxx
+            M[i,2*dimension] = 1
+            M[2*dimension,i+dimension] = 1
+            Z[i] = numpy.dot(x, y[i]) / length
+            Z[i+dimension] = numpy.mean(y[i])
+            sy = numpy.mean(y[i])
+            syy = numpy.dot(y[i], y[i]) / length
+            sxy = Z[i]
+            R[i] = (sxy - sx*sy) / numpy.sqrt((sxx-sx*sx) * (syy - sy*sy))
+        M[2*dimension,2*dimension] = -1
+        return numpy.matmul(numpy.linalg.inv(M), Z), R
 
     @staticmethod
-    def matrixes_lsf(mat, dimension, begin, end) :
+    def matrics_lsfs(mat, dimension, begin, end) :
         A = numpy.zeros([dimension,dimension])
         B = numpy.zeros([dimension,dimension])
         R = numpy.zeros([dimension,dimension])
+        x = numpy.zeros([end-begin+1])
         for i in range(dimension) :
-            for j in range(dimension) :
-                x = []
-                y = []
-                for k in range(begin, end+1) :
-                    x.append(k+1)
-                    y.append(mat[k][i][j])
-                A[i,j], B[i,j], R[i,j] = IGMENS.lsf_xy(x, y)
+            y = numpy.zeros([dimension, end-begin+1])
+            for k in range(begin, end+1) :
+                x[k-begin] = k+1
+                for j in range(dimension) :
+                    y[j][k-begin] = mat[k][i][j]
+            lsfs_xy_ret, lsfs_xy_r = IGMENS.lsfs_xy(x, y, dimension)
+            for l in range(dimension) :
+                A[i][l] = lsfs_xy_ret[l]
+                B[i][l] = lsfs_xy_ret[l+dimension]
+                R[i][l] = lsfs_xy_r[l]
         return A, B, R
 
     @staticmethod
@@ -121,6 +147,8 @@ class IGME(object) :
         The \hat{T} matrix
     lnA : array like, shape = (n_state, n_state)
         The logorithm of A matrix
+    A : array like, shape = (n_state, n_state)
+        The A matrix
     R : array like, shape = (n_state, n_state)
         A matrix of Pearson Correlation Coefficients
     rmse : real
@@ -129,10 +157,13 @@ class IGME(object) :
     timescales : array like, shape = (n_state - 1)
         The implied timescales of \hat{T} matrix. This is also the long-time
         limit of implied timescales of IGME
-    M0 : array like, shape = (n_state, n_state)
+    M0 : array like, shape = (2, n_state, n_state)
         The time integral of memory kernels.
-    mik : real
+        M0[0] = \dot{T}(0) - ln \hat{T} 
+        M0[1] = T(1)^-1 \dot{T}(1) - ln \hat{T} 
+    mik : array like, shape = (2)
         The root square mean of all elements of M0
+        mik[0] is computed from M0[0], mik[1] is computed from M0[1]
 
     Reference
     ----------
@@ -140,12 +171,15 @@ class IGME(object) :
     """
 
     def __init__(self, logarithm_approx_order=0) :
-        self.logarithm_approx_order = logarithm_approx_order
+        self._logarithm_approx_order = logarithm_approx_order
+        self._theory = 0
+        self._dim   = 0
         self._begin = 0
         self._end   = 0
         self._lnTh  = 0
         self._Th    = 0
         self._lnA   = 0
+        self._A     = 0
         self._R     = 0
         self._M0    = 0
         self._mik   = 0
@@ -153,8 +187,20 @@ class IGME(object) :
         self._rmse  = 0
 
     @property
+    def begin(self):
+        return self._begin
+
+    @property
+    def end(self):
+        return self._end
+
+    @property
     def lnTh(self):
         return self._lnTh
+
+    @property
+    def A(self):
+        return self._A
 
     @property
     def lnA(self):
@@ -224,15 +270,20 @@ class IGME(object) :
         self._end = end
         if (self._dim <= 0) :
             return 0
-        lnTPMs = IGMENS.matrixes_log(reshaped_input_data, self.logarithm_approx_order)
-        self._lnA, self._lnTh, self._R = IGMENS.matrixes_lsf(lnTPMs, self._dim, self._begin-1, self._end-1)
+        lnTPMs = IGMENS.matrics_log(reshaped_input_data, self._logarithm_approx_order)
+        self._lnA, self._lnTh, self._R = IGMENS.matrics_lsfs(lnTPMs, self._dim, self._begin-1, self._end-1)
+        self._A = IGMENS.matrix_exp(self._lnA)
         self._Th = IGMENS.matrix_exp(self._lnTh)
-        ev = -numpy.linalg.eigvals(self._lnTh)
+        ev = -numpy.real(numpy.linalg.eigvals(self._lnTh))
         ev.sort()
         self._timescales = (1 / ev[1:])
         self._rmse = self._compute_rmse(reshaped_input_data, rmse_weighted_by_sp)
-        self._M0 = IGMENS.matrix_log(reshaped_input_data[0], self.logarithm_approx_order) - self._lnTh
-        self._mik = numpy.linalg.norm(self._M0) / self._dim
+        self._M0 = [ 0, 0 ]
+        self._M0[0] = IGMENS.matrix_log(reshaped_input_data[0], self._logarithm_approx_order) - self._lnTh
+        self._M0[1] = numpy.matmul(numpy.linalg.inv((reshaped_input_data[0] + reshaped_input_data[1])*0.5), reshaped_input_data[1] - reshaped_input_data[0]) - self._lnTh
+        self._mik = [ 0, 0 ]
+        self._mik[0] = numpy.linalg.norm(self._M0[0]) / self._dim
+        self._mik[1] = numpy.linalg.norm(self._M0[1]) / self._dim
         return self
 
     def predict(self, begin, end) :
@@ -257,7 +308,13 @@ class IGME(object) :
             return 0
         ret = [];
         for i in range(begin-1, end) :
-            ret.append(IGMENS.matrix_exp(numpy.multiply(self._lnTh, i) + self._lnA))
+            if (self._theory == 2) :
+                ret.append(numpy.matmul(self._A, IGMENS.matrix_exp(numpy.multiply(self._lnTh, i))))
+            elif (self._theory == 3) :
+                Asqrt = IGMENS.matrix_exp(numpy.multiply(self._lnA, 0.5))
+                ret.append(numpy.matmul(Asqrt, numpy.matmul(IGMENS.matrix_exp(numpy.multiply(self._lnTh, i)), Asqrt)))
+            else :
+                ret.append(IGMENS.matrix_exp(numpy.multiply(self._lnTh, i) + self._lnA))
         return ret
 
     def _compute_rmse(self, data, rmse_weighted_by_sp=True) :
@@ -265,7 +322,7 @@ class IGME(object) :
             return 0
         diff = self.predict(1, len(data)) - data
         if (rmse_weighted_by_sp) :
-            sp_matrix = numpy.diag(IGMENS.matrix_exp(numpy.multiply(IGMENS.matrix_log(data[int((self._begin+self._end)/2)], self.logarithm_approx_order), 1e9).reshape([self._dim, self._dim]))[0]);
+            sp_matrix = numpy.diag(IGMENS.matrix_exp(numpy.multiply(IGMENS.matrix_log(data[int((self._begin+self._end)/2)], self._logarithm_approx_order), 1e9).reshape([self._dim, self._dim]))[0]);
             for i in range(len(diff)) :
                 diff[i] = numpy.matmul(sp_matrix, diff[i])
         return numpy.sqrt(numpy.square(numpy.mean(diff)) + numpy.square(numpy.std(diff)))
@@ -277,6 +334,8 @@ class IGME(object) :
         dic['lnTh']     = []
         dic['Th']       = []
         dic['lnA']      = []
+        dic['A']        = []
+        dic['R']        = []
         dic['rmse']     = []
         dic['timescales'] = []
         dic['M0']       = []
@@ -307,10 +366,41 @@ class IGME(object) :
         dic['lnTh'].append(self._lnTh)
         dic['Th'].append(self._Th)
         dic['lnA'].append(self._lnA)
+        dic['A'].append(self._A)
+        dic['R'].append(self._R)
         dic['rmse'].append(self._rmse)
         dic['timescales'].append(self._timescales)
         dic['M0'].append(self._M0)
         dic['mik'].append(self._mik)
+        return dic
+
+    def _from_output(self, dic, index=0) :
+        """ copy IGME model from output dictionary: dic[index]
+
+        Parameters:
+        ----------
+        dic : dictionary
+            The dictionary of output(s) generated with IGME.output()
+        index : int
+            The element in dic to copy 
+
+        Returns
+        -------
+        self : object
+            returns the instance itself
+        """
+        self._dim        = len(dic['Th'][index])
+        self._begin      = dic['begin'][index]
+        self._end        = dic['end'][index]
+        self._lnTh       = dic['lnTh'][index]
+        self._Th         = dic['Th'][index]
+        self._lnA        = dic['lnA'][index]
+        self._A          = dic['A'][index]
+        self._R          = dic['R'][index]
+        self._M0         = dic['M0'][index]
+        self._mik        = dic['mik'][index]
+        self._timescales = dic['timescales'][index]
+        self._rmse       = dic['rmse'][index]
         return dic
 
     def fit_output(self, input_data, begin, end, rmse_weighted_by_sp=True) :
@@ -353,13 +443,14 @@ class IGME(object) :
         if (end<=begin) | (end>len(out['begin'])-1) :
             end = len(out['begin']) - 1
         for i in range(begin, end+1) :
+            dim = len(out['lnTh'][i])
             print(out['begin'][i], end=comma, file=file)
             print(out['end'][i], end=comma, file=file)
             print(out['rmse'][i], end=comma, file=file)
             self._print_number_array(out['timescales'][i], end=comma, file=file)
-            self._print_number_array(out['lnTh'][i].reshape([self._dim*self._dim]), end=comma, file=file)
-            self._print_number_array(out['lnA'][i].reshape([self._dim*self._dim]), end=comma, file=file)
-            print(out['mik'][i], end='\n', file=file)
+            self._print_number_array(out['lnTh'][i].reshape([dim*dim]), end=comma, file=file)
+            self._print_number_array(out['lnA'][i].reshape([dim*dim]), end=comma, file=file)
+            print("'"+str(out['mik'][i][0])+" "+str(out['mik'][i][1])+"'", end='\n', file=file)
 
     def scan(self, input_data, begin, end, stride=1, rmse_weighted_by_sp=True, debug=False) :
         """ Scan IGME models within the given range of lagtime
@@ -401,4 +492,84 @@ class IGME(object) :
                     self.print_output(output, begin=len(output['begin'])-1, _print_title=_print_title)
                     _print_title = False
         return output
+
+    def top_outputs(self, dic, n=1) :
+        """ Extract the top models of an output dictionary.
+        Top models are defined by smallest RMSEs.
+
+        Parameters
+        ----------
+        dic: dictionary
+            The full dictionary
+        n: int or float
+            The top models to output
+            n>=1 : the number of models
+            n<1 : the percentage of original data
+
+        Return
+        ------
+        output: dictionary
+            returns a dictory of top elements of input dictionary
+
+        """
+        total_rec = len(dic['Th'])
+        n_rec = n
+        if n < 1 :
+            n_rec = numpy.array(numpy.ceil(total_rec * n)).astype(numpy.int)
+        if n_rec < 1 :
+            n_rec = 1
+        if n_rec >= total_rec :
+            return dic
+        ind_rmse = numpy.argsort(dic['rmse'])
+        ret = self._init_output_dic()
+        temp = IGME()
+        for i in range(n_rec) :
+            temp._from_output(dic, ind_rmse[i])
+            temp.output(ret)
+        return ret
+
+    def _median_outputs(self, dic) :
+        """Find the median model, closest to the average ln\hat{T}
+
+        Parameters
+        ----------
+        dic: dictionary
+            The full dictionary
+
+        Return
+        ------
+        output: dictionary
+            returns a dictory of top elements of input dictionary
+        """
+        Th_average = numpy.mean(dic['lnTh'], axis=0)
+        rmse_list = [0] * len(dic['lnTh'])
+        for i in range(len(dic['lnTh'])) :
+            rmse_list[i] = numpy.linalg.norm(dic['lnTh'][i] - Th_average)
+        ind_rmse = numpy.argsort(rmse_list)
+        ret = IGME()
+        ret._from_output(dic, ind_rmse[0])
+        return ret
+
+    def top_model(self, dic, n=1) :
+        """Find the median model of top models
+        Top models have the smallest RMSEs
+        The median model has \ln\hat{T} closest to average
+
+        Parameters
+        ----------
+        dic: dictionary
+            The full dictionary
+        n: int or float
+            The top models to output
+            n>=1 : the number of models
+            n<1 : the percentage of original data
+
+        Return
+        ------
+        output: object 
+            returns the best IGME object
+        """
+        result = self.top_outputs(dic, n)
+        ret = self._median_outputs(result)
+        return ret
 
